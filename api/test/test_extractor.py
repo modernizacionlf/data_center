2182a6f1b7952bb218b9766376e8297e7d622fd2
agusherrera99 <agustinherrera.dev@gmail.com>
2025-08-08 12:40:46 -0300

    Update database connection string in test setup to use environment variables

--- api/test/test_extractor.py
+++ api/test/test_extractor.py
@@ -10,13 +10,15 @@ from src.paths import GEONODE_ENV_PATH
 class TestDatabaseExtractor(TestCase):
     def setUp(self) -> None:
         load_dotenv(GEONODE_ENV_PATH)
+        DB_USER = os.getenv("DB_USER")
         DB_PASSWORD = os.getenv("DB_PASSWORD")
         DB_HOST = os.getenv("DB_HOST")
         DB_PORT = os.getenv("DB_PORT")
+        DB_NAME = os.getenv("DB_NAME")
 
         source_config = {
             "name": "geonode",
-            "connection_string": f"postgresql://geonode:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/geonode"
+            "connection_string": f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
         }
         self.database_extractor = DatabaseExtractor(source_config)
 
