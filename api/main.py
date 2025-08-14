507dbf014b24800f4917141f67b66113adfc081e
agusherrera99 <agustinherrera.dev@gmail.com>
2025-08-14 12:34:50 -0300

    Fix variable name from 'app' to 'api' for consistency in main.py

--- api/main.py
+++ api/main.py
@@ -3,16 +3,16 @@ from fastapi import FastAPI
 from src.endpoints import ENDPOINTS
 
 
-app = FastAPI(title="Centro de Datos - Las Flores")
+api = FastAPI(title="Centro de Datos - Las Flores")
 
-@app.get(ENDPOINTS.BASE)
+@api.get(ENDPOINTS.BASE)
 async def root():
     return "Hello world"
 
-@app.get(ENDPOINTS.ENTITIES)
+@api.get(ENDPOINTS.ENTITIES)
 async def get_entities():
     return "Entidades disponibles"
 
-@app.get(ENDPOINTS.ENTITY("{entity_name}"))
+@api.get(ENDPOINTS.ENTITY("{entity_name}"))
 async def get_entity(entity_name: str):
     return {"Entity": entity_name}
