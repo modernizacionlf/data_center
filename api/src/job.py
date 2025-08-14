5a692e5e4af79addf86a82830d900601c7faa80e
agusherrera99 <agustinherrera.dev@gmail.com>
2025-08-14 00:55:27 +0000

    Refactor DatabaseJob.run method to initialize loaders and transformer; improve code structure and readability

--- api/src/job.py
+++ api/src/job.py
@@ -3,6 +3,9 @@ from typing import Sequence
 from src.extractor import DatabaseExtractor
 from src.monitoring import CronJobMonitor
 from src.pipeline import DataPipeline
+from src.staging import StagingLoader
+from src.transform import DataTransformer
+from src.warehouse import WarehouseLoader
 from utils.db_connections import DBConnection, DataCenter, Geonode
 from utils.paths import DATA_CENTER_PRODUCTION_PATH, GEONODE_ENV_PATH
 
@@ -15,15 +18,21 @@ class DatabaseJob():
 
     def run(self):
         for database in self.dbconnections:
-            extractor = DatabaseExtractor(database.source_config)
-            pipeline = DataPipeline(target_connection_string=datacenter.connection_string)
+            staging_loader = StagingLoader(datacenter.connection_string)
+            warehouse_loader = WarehouseLoader(datacenter.connection_string)
+            transformer = DataTransformer()
+            
+            pipeline = DataPipeline(
+                staging_loader,
+                warehouse_loader,
+                transformer
+            )
 
+            extractor = DatabaseExtractor(database.source_config)
             for query in database.queries:
                 pipeline.run(
                     extractor,
                     extract_kwargs=query,
-                    staging_schema="staging",
-                    final_schema="warehouse"
                 )
 
 
