656f6613c75f8e6db891dc79bee2048bc52b06d0
agusherrera99 <agustinherrera.dev@gmail.com>
2025-08-14 10:37:59 -0300

    Refactor ROOT_PATH to point real root project; add API_ROOT to point API dir

--- api/utils/paths.py
+++ api/utils/paths.py
@@ -1,6 +1,7 @@
-from utils.rootpath import get_project_root_path
+from api.utils.rootpath import get_project_root_path
 
-ROOT_PATH = get_project_root_path()
-GEONODE_ENV_PATH = ROOT_PATH / ".env.geonode"
-DATA_CENTER_ENVIRONMENT_PATH = ROOT_PATH.parent / ".env.development"
-DATA_CENTER_PRODUCTION_PATH = ROOT_PATH.parent / ".env.production"
+ROOT_PATH = get_project_root_path().parent
+API_ROOT = ROOT_PATH / "api"
+GEONODE_ENV_PATH = API_ROOT / ".env.geonode"
+DATA_CENTER_DEVELOPMENT_PATH = ROOT_PATH / ".env.development"
+DATA_CENTER_PRODUCTION_PATH = ROOT_PATH / ".env.production"
