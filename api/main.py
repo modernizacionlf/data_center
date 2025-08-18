29a59e5b1e175c58a46e6edefdb266b36eca2e81
agusherrera99 <agustinherrera.dev@gmail.com>
2025-08-18 13:50:43 -0300

    Refactor API endpoints to utilize get_available_entities for entity retrieval and remove unused root endpoint

--- api/main.py
+++ api/main.py
@@ -1,18 +1,15 @@
 from fastapi import FastAPI
 
-from src.endpoints import ENDPOINTS
+from utils.endpoints import ENDPOINTS, get_available_entities
 
 
 api = FastAPI(title="Centro de Datos - Las Flores")
 
-@api.get(ENDPOINTS.BASE)
-async def root():
-    return "Hello world"
-
 @api.get(ENDPOINTS.ENTITIES)
 async def get_entities():
-    return "Entidades disponibles"
+    entities = get_available_entities()
+    return {"entidades": entities}
 
 @api.get(ENDPOINTS.ENTITY("{entity_name}"))
 async def get_entity(entity_name: str):
-    return {"Entity": entity_name}
+    return {"entidad": entity_name}
