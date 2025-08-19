a10fffe5de9fb6698ed64fc53fc485932f334bb7
agusherrera99 <agustinherrera.dev@gmail.com>
2025-08-19 11:04:19 -0300

    Add STATISTICS method to ENDPOINTS class and refactor get_available_entities function

--- api/utils/endpoints.py
+++ api/utils/endpoints.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from src.extractor import DatabaseExtractor, QueryRequest
 from utils.db_connections import DataCenter
 from utils.paths import DATA_CENTER_PRODUCTION_PATH
@@ -6,14 +8,20 @@ from utils.paths import DATA_CENTER_PRODUCTION_PATH
 class ENDPOINTS:
     BASE = "/api"
     ENTITIES = f"{BASE}/entidades"
-    
+
     @classmethod
     def ENTITY(cls, entity_name: str):
         return f"{cls.ENTITIES}/{entity_name}"
 
+    @classmethod
+    def STATISTICS(cls, entity_name: str):
+        return f"{cls.ENTITIES}/{entity_name}/estadisticas"
+
+
+datacenter = DataCenter(DATA_CENTER_PRODUCTION_PATH)
+extractor = DatabaseExtractor(datacenter.source_config)
 
 def get_available_entities() -> list[str]:
-    datacenter = DataCenter(DATA_CENTER_PRODUCTION_PATH)
     query_request = QueryRequest(
         query="""
             SELECT table_name
@@ -23,7 +31,25 @@ def get_available_entities() -> list[str]:
 	        ORDER BY table_name
         """
     )
-    extractor = DatabaseExtractor(datacenter.source_config)
     enitites_dataframe = extractor.extract(query_request)
     entities = enitites_dataframe["table_name"].to_list()
     return entities
+
+def get_available_statistics(entity_name: str) -> dict[str, list[dict[str, Any]]]:
+    stats: dict[str, list[dict[str, Any]]] = {}
+    excluded_columns = ["_source", "_batch_id", "_extracted_at", "record_hash"]
+    query_request = QueryRequest(
+        query=f'SELECT * from warehouse."{entity_name}"',
+        params={"table": entity_name}
+    )
+    entity_dataframe = extractor.extract(query_request)
+    entity_dataframe = entity_dataframe.drop(columns=excluded_columns, errors="ignore")
+    available_columns = entity_dataframe.columns.tolist()
+
+    for column in available_columns:
+        if entity_dataframe[column].dtype in ["object", "bool"] or entity_dataframe[column].nunique() < 20:
+            grouped_df = entity_dataframe.groupby(column).size().reset_index(name="cantidad") # type: ignore
+            grouped_df = grouped_df.rename(columns={column: "valor"})
+            stats[column] = grouped_df.to_dict('records') # type: ignore
+
+    return stats
