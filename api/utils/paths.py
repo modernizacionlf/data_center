d052c9889e776716edecfcb70f00124a65a6e864
agustin herrera <agustinherrera.dev@gmail.com>
2025-08-22 17:55:24 -0300

    Refactor change rafam path to base unica path

--- api/utils/paths.py
+++ api/utils/paths.py
@@ -4,6 +4,6 @@ ROOT_PATH = get_project_root_path().parent
 API_PATH = ROOT_PATH / "api"
 SECRETS = API_PATH / "secrets"
 GEONODE_ENV_PATH = SECRETS / ".env.geonode"
-RAFAM_CD_ENV_PATH = SECRETS / ".env.rafam.cd"
+BASE_UNICA_ENV_PATH = SECRETS / ".env.baseunica"
 DATA_CENTER_DEVELOPMENT_PATH = API_PATH / ".env.development"
 DATA_CENTER_PRODUCTION_PATH = API_PATH / ".env.production"
