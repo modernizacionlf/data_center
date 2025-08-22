1be01a25a6e93d59770b530491d89c8b9f21b1fc
agustin herrera <agustinherrera.dev@gmail.com>
2025-08-22 17:56:54 -0300

    Create BaseUnica class DBConnection

--- api/utils/db_connections.py
+++ api/utils/db_connections.py
@@ -65,7 +65,7 @@ class Geonode(DBConnection):
         return [
             QueryRequest(
                 query="""
-                    SELECT id, estado, nivel, pintura 
+                    SELECT r.id, r.estado, r.nivel, r.pintura 
                     FROM global.rampas r
                     JOIN global.rampas_estados re ON r.estado_id = re.estado_id
                     JOIN global.rampas_niveles rn ON r.nivel_id = rn.nivel_id
@@ -73,6 +73,40 @@ class Geonode(DBConnection):
                 main_table="rampas"
             )
         ]
+
+
+class BaseUnica(DBConnection):
+    def __init__(
+        self,
+        env_path: Path,
+        user_key: str = "DB_USER",
+        password_key: str = "DB_PASSWORD",
+        host_key: str = "DB_HOST",
+        port_key: str = "DB_PORT",
+        db_key: str = "DB_NAME",
+    ) -> None:
+        super().__init__(env_path, user_key, password_key, host_key, port_key, db_key)
+
+    @property
+    def queries(self) -> list[QueryRequest]:
+        return [
+            QueryRequest(
+                query="""
+                    SELECT cm.sexo, cm.edad, cm.obra_social FROM consultas_medicas cm
+                    WHERE cm.obra_social NOT IN ('ASOCIACION MUTUAL FARMACEUTICOS FLORENTINO AMEGHINO', 'ASOCIACION MUTUALISTA DE EMPLEADOS DEL BANCO PROVINCIA DE BUENOS AIRES', 'PROGRAMA INCLUIR SALUD', 'PROGRAMA SUMAR', 'PROGRAMA FEDERAL DE SALUD')
+                    AND cm.edad > 0 AND cm.edad < 103
+                """,
+                main_table="centros_de_atencion_primaria"
+            ),
+            QueryRequest(
+                query="""
+                    SELECT i.falta, a.tipo_vehiculo, COALESCE(a.retiene_licencia, false) AS retiene_licencia, COALESCE(a.retiene_vehiculo, false) AS retiene_vehiculo
+                    FROM actas a INNER JOIN imputaciones i ON a.id = i.id_acta
+                """,
+                main_table="infracciones"
+            )
+        ]
+
 
 
 class DataCenter(DBConnection):
