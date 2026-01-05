936e6098d844c508627059a1e138767c28204b47
agusherrera99 <agustinherrera.dev@gmail.com>
2026-01-05 11:23:21 -0300

    Fix 'dot' missing in endpoints route import

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -1,7 +1,7 @@
 from .paths import BASE_UNICA_ENV_PATH, DATA_CENTER_PRODUCTION_PATH, GEONODE_ENV_PATH
 from .db_connections import DBConnection, DataCenter, Geonode, BaseUnica, QueryRequest
 from .rootpath import get_project_root_path
-from endpoints import ENDPOINTS, get_available_entities, get_available_statistics
+from .endpoints import ENDPOINTS, get_available_entities, get_available_statistics
 
 __all__ = [
     BASE_UNICA_ENV_PATH, DATA_CENTER_PRODUCTION_PATH, GEONODE_ENV_PATH,
